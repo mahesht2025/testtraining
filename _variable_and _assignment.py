2a67f29ded4aab0e909ac1101d88606d27540145
mahesht2025 <maheshthota2025@gmail.com>
2024-04-30 16:20:44 +0530

    re-assign a different integer to the first variable you created

--- _variable_and _assignment.py
+++ _variable_and _assignment.py
@@ -5,3 +5,11 @@ print("integer value :",rv)
 #create a second variable and assign it a float 
 decimal_value = 12.9876
 print("decimal value :",decimal_value)
+
+#create a third variable and assign it a Boolean value
+boolean_val =True
+print("Boolean values :",boolean_val)
+
+#re-assign a different integer to the first variable you created
+rv =111
+print("re assigning value :",rv)
