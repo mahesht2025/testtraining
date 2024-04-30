a856251d25cde92a9e40a38f0a37278f1ea6f0c3
mahesht2025 <maheshthota2025@gmail.com>
2024-04-30 16:09:06 +0530

    create a second variable and assign it a float

--- _variable_and _assignment.py
+++ _variable_and _assignment.py
@@ -1,3 +1,7 @@
 #create a variable and assign the integer value
 rv = 10
-print(rv)
+print("integer value :",rv)
+
+#create a second variable and assign it a float 
+decimal_value = 12.9876
+print("decimal value :",decimal_value)
