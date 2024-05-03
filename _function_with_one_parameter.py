038ecf5a9da9c54a174a5798d39db01805d2f742
mahesht2025 <maheshthota2025@gmail.com>
2024-05-03 14:15:53 +0530

    using functions with one parameter with using user assignment

---
+++ _function_with_one_parameter.py
@@ -0,0 +1,9 @@
+#Create a function called name_printer which takes 1 parameter and prints it
+def data_printer(user_name):
+    print(user_name)
+
+#Create a variable called name and assign it user input().  input()'s message should ask the user to enter their name.
+name= input("please enter your name :")
+
+#Call name_printer() with the variable "name" as its argument.
+data_printer(name)
