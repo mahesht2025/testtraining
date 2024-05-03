aac554f15234035c71bf7f0438753ddea1b20429
mahesht2025 <maheshthota2025@gmail.com>
2024-05-03 14:38:06 +0530

    to convert the celcius to fahrehiet solution of program

---
+++ celsius_for_fahrenheit.py
@@ -0,0 +1,9 @@
+celsius = int(input(" enter the value for degrees celsius. "))
+ 
+ 
+def fahrenheit(cel):
+    
+    return (18 * cel + 320) / 10
+ 
+ 
+print("The Fahrenheit equivalent of " + str(celsius) + " degrees Celsius is " + str(fahrenheit(celsius)) + ".")
