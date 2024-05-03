d19fc16ce5529b3a886ca5bad77f621dbc31d108
mahesht2025 <maheshthota2025@gmail.com>
2024-05-03 13:58:18 +0530

    Call the function hello_world_printer()

--- _funtion_with_no_parameters.py
+++ _funtion_with_no_parameters.py
@@ -1,3 +1,6 @@
+#Create a function called hello_world_printer() which takes no parameters and prints the string "hello world"
 def hello_world_printer():
     print("Hello World....")
+ 
+ #Call the function hello_world_printer()   
 hello_world_printer()    
