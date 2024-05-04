d93f4f138b8ee758e5ea3a82ef5251e99fb85c73
mahesht2025 <maheshthota2025@gmail.com>
2024-05-04 08:52:23 +0530

    Programming Challenge for  Roman Numeral Equivalent

---
+++ _roman_number_equivalent.py
@@ -0,0 +1,23 @@
+import random
+rv = random.randint(1, 10)
+
+if rv==1:
+    print("The roman number equivalent of " + str(rv) + " is I.")
+elif rv==2:
+    print("The roman number equivalent of " + str(rv) + " is II.")
+elif rv==3:
+    print("The roman number equivalent of " + str(rv) + " is III.")
+elif rv==4:
+    print("The roman number equivalent of " + str(rv) + " is IV.")
+elif rv==5:
+    print("The roman number equivalent of " + str(rv) + " is V.")
+elif rv==6:
+    print("The roman number equivalent of " + str(rv) + " is VI.")
+elif rv==7:
+    print("The roman number equivalent of " + str(rv) + " is VII.")
+elif rv==8:
+    print("The roman number equivalent of " + str(rv) + " is VIII.")
+elif rv==9:
+    print("The roman number equivalent of " + str(rv) + " is IX.")
+elif rv==10:
+    print("The roman number equivalent of " + str(rv) + " is X.")
