021cefe714568f7f8232d7f787198b7c26f97cd8
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 14:46:20 +0530

    using int() for printing integer value to con cate in the print method

---
+++ int()_excercise.py
@@ -0,0 +1,4 @@
+#In a Python file, use input() to ask the user to enter an integer that 10 will be added to.  Assign what they type to a variable.
+
+inte = int(input("Enter the  integer value : "))
+print(inte + 10)
