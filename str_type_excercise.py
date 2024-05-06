b70c8defa103fbf60d487cbda8ce84f49f74e1dd
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 14:20:11 +0530

    Use str() on the variable from sd concatenate it with the string

--- str_type_excercise.py
+++ str_type_excercise.py
@@ -2,7 +2,11 @@
 
 flt_val=4.5
 
-#Create a variable and assign it a float
+#Use print() and type() to print the data type of the variable in the output
+
 
 print(type(flt_val))
 
+#Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
+
+print(str(flt_val)+ " is a float value")
