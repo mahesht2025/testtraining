ebd33c3f45e3b398d3a08f331b3c4ef626b22052
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 14:38:30 +0530

    monty python code to print()

---
+++ monty_python.py
@@ -0,0 +1,6 @@
+#Programming Challenge: Monty Python
+name = input("Enter you name : ")
+quest = input("Enter you quest : ")
+color = input("Enter you fav color: ")
+
+print("your name is "+name+ ", your quest is "+quest+ " , and you favorite colour is "+color)
