8abd153d96cc9fe7800559e864c8f17b3bd62f3c
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 15:17:57 +0530

    checking the string is upper case or not while using the isupper()

--- string_2_excercises.py
+++ string_2_excercises.py
@@ -1,2 +1,6 @@
 #Create a variable called mixed_case and assign it the string "A Song of Ice and Fire"
 mixed_case = "A Song of Ice and Fire"
+
+#Use .isupper() to check if mixed_case is a string of all upper case letters.  print() the result.
+
+print(mixed_case.isupper())
