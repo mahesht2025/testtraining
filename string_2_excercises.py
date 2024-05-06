e40e20ad1dd10ad8820fed6caf913b2453c8904f
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 15:20:01 +0530

    checking the string is upper case or not while using the islower()

--- string_2_excercises.py
+++ string_2_excercises.py
@@ -4,3 +4,6 @@ mixed_case = "A Song of Ice and Fire"
 #Use .isupper() to check if mixed_case is a string of all upper case letters.  print() the result.
 
 print(mixed_case.isupper())
+
+#Use .islower() to check if mixed_case is a string of all lower case letters.  print() the result.
+print(mixed_case.islower())
