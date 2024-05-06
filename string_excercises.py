a0ef88d36e577ada8a6a53d83ef3cf4ab56e2ec2
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 14:05:29 +0530

    you slice and concate the string

--- string_excercises.py
+++ string_excercises.py
@@ -12,3 +12,6 @@ print(ster[11:])
 
 #Print the slice "never" from the variable
 print(ster[0:5])
+
+#Get the string slice "give up" from the variable and concatenate it with the string "i will ".  Print the resulting string, which should be "i will give up" where the "give up!" part is a slice.
+print("i will " + ster[6:])
