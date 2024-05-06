961d7134a7e4845828fdfe94bdbd34f9e80f6ace
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 14:25:44 +0530

    Uses the escape for the string

--- str_type_excercise.py
+++ str_type_excercise.py
@@ -10,3 +10,7 @@ print(type(flt_val))
 #Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
 
 print(str(flt_val)+ " is a float value")
+
+#print() the string "Hello, I'm [name], it's nice to meet you!" including quotes (you will need to use the \' or \" escape sequence depending on whether you enclose your strings in single quotes or double quotes.)
+
+print("\"Hello, I'm Mahesh ,it's nice to meet you!\"")
