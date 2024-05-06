ce477a86359aec080960f4edfec21f9162c00067
mahesht2025 <maheshthota2025@gmail.com>
2024-05-06 13:55:02 +0530

    Access the p from the variable by index and print() it

--- string_excercises.py
+++ string_excercises.py
@@ -1,2 +1,5 @@
-#Create a variable and assign it the string "Just do it!"
+#Create a variable and assign it the string 
 ster = "never give up"
+
+#Access the "p" from the variable by index and print() it
+print(ster[12])
