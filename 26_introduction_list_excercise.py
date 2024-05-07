d525e01a092815a6cf7327b02707ce39e7418795
mahesht2025 <maheshthota2025@gmail.com>
2024-05-07 13:41:22 +0530

    Create another variable and assign it a call of the list() function with a string as its argument

--- 26_introduction_list_excercise.py
+++ 26_introduction_list_excercise.py
@@ -1,2 +1,5 @@
 #Create a variable and assign it a list that contains an integer, a float, a Boolean value, a string, and a list of 3 integers.
 man =[2,90.8,True,"Thota",[4,8,7]]
+
+#Create another variable and assign it a call of the list() function with a string as its argument.
+alist = list("covid")
