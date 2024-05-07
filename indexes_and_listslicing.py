6ef4235605b24dfa7c00692c0b431586423ed812
mahesht2025 <maheshthota2025@gmail.com>
2024-05-07 14:07:46 +0530

    Create a second variable and assign it the list

--- indexes_and_listslicing.py
+++ indexes_and_listslicing.py
@@ -6,3 +6,6 @@ print(man[0])
 
 #Access the 14 from the list in step 1 then print it.
 print(man[3][1])
+
+#Create a second variable and assign it the list ["chair", "table", "desk", "lamp", "bed"]
+furn=["chair", "table", "desk", "lamp", "bed"]
