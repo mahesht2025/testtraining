59c164812eba954435c84ed7c90faecd3e709e10
mahesht2025 <maheshthota2025@gmail.com>
2024-05-07 14:48:36 +0530

    Use del to remove tiger from the list assigned to arctic_animals

--- 28_del_and_list.py
+++ 28_del_and_list.py
@@ -1,2 +1,5 @@
 #Create a variable called arctic_animals and assign it the list ["penguin", "elephant", "polar bear", "walrus", "tiger", "reindeer"]
 arctic_animals=["penguin", "elephant", "polar bear", "walrus", "tiger", "reindeer"]
+
+#Use del to remove "tiger" from the list assigned to arctic_animals.
+del arctic_animals[4]
