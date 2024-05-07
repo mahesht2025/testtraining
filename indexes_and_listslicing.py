997165cd60f22c6b358e7f924a8d60d5d7210246
mahesht2025 <maheshthota2025@gmail.com>
2024-05-07 14:02:30 +0530

    Access the first list from the list of lists

---
+++ indexes_and_listslicing.py
@@ -0,0 +1,5 @@
+#Create a variable and assign it the list [[0, 2], [4, 6], [8, 10], [12, 14]]
+man = [[0,2],[4,6],[8,10],[12,14]]
+
+#Access the first list from the list of lists in step 1 by index then print it.
+print(man[0])
