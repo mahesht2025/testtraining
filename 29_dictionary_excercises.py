bfaf9e20bc8161d88fddc7d43e5b97cd067d1bce
mahesht2025 <maheshthota2025@gmail.com>
2024-05-08 16:15:20 +0530

    use the in keyword to check if a key appears in the dictionary and print the result

--- 29_dictionary_excercises.py
+++ 29_dictionary_excercises.py
@@ -3,3 +3,6 @@ rv={'a':1,'b':2,'c':3,'d':4}
 
 #print and access the value held at the third key in the dictionary
 print(rv['c'])
+
+#use the in keyword to check if a key appears in the dictionary and print the result
+print('d' in rv)
