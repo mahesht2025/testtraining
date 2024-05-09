0dfe7d3075352f5ac2e79bf30646b535634f72b0
mahesht2025 <maheshthota2025@gmail.com>
2024-05-09 08:39:32 +0530

    use .update() to add the contents of another_one to internet_celebrities.

--- 32_dictionary_exce3.py
+++ 32_dictionary_exce3.py
@@ -1,3 +1,6 @@
 #paste these 2 dictionaries into your file
 internet_celebrities = {"DrDisrespect": "YouTube", "ZLaner": "Facebook", "Ninja": "Mixer"}
 
+#use .update() to add the contents of another_one to internet_celebrities.
+another ={"Mahesh":"Dream11"}
+internet_celebrities.update(another)
