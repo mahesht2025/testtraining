944bda422bca56c00416a412e0892337c043239c
mahesht2025 <maheshthota2025@gmail.com>
2024-05-09 08:25:40 +0530

    paste this dictionary into your .py file then pop and print Big Mac from it

--- 31_dictionary_exce2.py
+++ 31_dictionary_exce2.py
@@ -1,3 +1,6 @@
 #use .fromkeys() to create a dictionary that has the consonants from the alphabet as its keys and the string "consonant" as the value for each of those keys.  Only use lower case letters for the consonants.  "y" counts as a consonant for this exercise.  Use a for loop and the .items() method to print each of those key: value pairs on a different line.  For example, the first 3 lines in the output should be the following:
 for key,value in {}.fromkeys("abcdefghijkl",'consonants').items():
     print(key,value)
+#paste this dictionary into your .py file then pop and print "Big Mac" from it
+fast_food_items = {"McDonald's": "Big Mac", "Burger King": "Whopper", "Chick-fil-A": "Original Chicken Sandwich"}
+print(fast_food_items.pop("McDonald's"))
