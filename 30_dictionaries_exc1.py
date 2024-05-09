ebeece4223284aaa7e48c6d8c3688d575ae11a96
mahesht2025 <maheshthota2025@gmail.com>
2024-05-09 07:57:41 +0530

    create a variable and assign it the following dictionary

---
+++ 30_dictionaries_exc1.py
@@ -0,0 +1,6 @@
+#create a variable and assign it the following dictionary:
+songs_name={"Queen": "Bohemian Rhapsody",
+     "Bee Gees": "Stayin' Alive",
+     "U2": "One", "Michael Jackson": "Billie Jean",
+     "The Beatles": "Hey Jude", "Bob Dylan": "Like A Rolling Stone"}
+
