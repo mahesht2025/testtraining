704057c013c5783a0f1fe16bd475ba735df5c82f
mahesht2025 <maheshthota2025@gmail.com>
2024-05-21 14:48:15 +0530

    Statements Excercise

--- 02_Course_C_Statement_test.py
+++ 02_Course_C_Statement_test.py
@@ -25,7 +25,7 @@ for i in st.split():
 #Write a program that prints the integers from 1 to 100. But for 
 #multiples of three print "Fizz" instead of the number, and for the multiples 
 #of five print "Buzz". For numbers which are multiples of both three and 
-# five print "FizzBuzz".
+# five print "FizzBuzz" .
 for i in range(1,100):
     if i%3==0:
         print("Fizz")
